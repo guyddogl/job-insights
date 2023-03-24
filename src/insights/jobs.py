4abfb78624f9953e9deb1ee3beab967413e71591
Guyddo <guyddogl@gmail.com>
2023-03-24 16:31:25 -0300

    Requirement 6

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,18 +5,6 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
     with open(path) as file:
         data = csv.DictReader(file)
         datafile = []
@@ -26,38 +14,9 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
     jobs = read(path)
-    types = {job["job_type"] for job in jobs}
-    return types
+    return {type["job_type"] for type in jobs}
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    return [job for job in jobs if job["job_type"] == job_type]
