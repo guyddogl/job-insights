5e57d70156fb2712164724611f523740d9673938
Guyddo <guyddogl@gmail.com>
2023-03-24 15:32:08 -0300

    Requirement 2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -40,7 +40,9 @@ def get_unique_job_types(path: str) -> List[str]:
     list
         List of unique job types
     """
-    raise NotImplementedError
+    jobs = read(path)
+    types = {job["job_type"] for job in jobs}
+    return types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
