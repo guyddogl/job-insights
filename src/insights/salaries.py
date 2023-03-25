006f286f42956e1de41a4a4c951a9c25b4a5a940
Guyddo <guyddogl@gmail.com>
2023-03-24 21:02:28 -0300

    Requirement 8

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -23,29 +23,28 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        if (
+            not str(job["max_salary"]).isnumeric()
+            or not str(job["min_salary"]).isnumeric()
+            or int(str(job["max_salary"])) < int(str(job["min_salary"]))
+        ):
+            raise TypeError()
+        else:
+            return (
+                int(str(job["min_salary"]))
+                <= int(salary)
+                <= int(str(job["max_salary"]))
+            )
+    except TypeError:
+        raise ValueError(
+            """O valores não são números ou o salário máximo
+             é menor do que o salário mínimo"""
+        )
+    except KeyError:
+        raise ValueError(
+            "A chave min_salary ou max_salary não foi localizada no dicionário"
+        )
 
 
 def filter_by_salary_range(
